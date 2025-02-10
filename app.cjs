c66814b5f5d5b4d4848252146a5cde70b9ed7d9c
Eric Theise <erictheise@gmail.com>
2025-02-10 15:27:30 -0600

    Interim commit

---
+++ app.cjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
